4c2ca20c38b4e7950135da2430c746a180a8d984
Jonathan Dang <jonakyd@gmail.com>
2013-01-14 11:04:34 +0800

    remove wrong package.json line

--- run.js
+++ run.js
@@ -1,8 +1,9 @@
 var express          = require('express')
-  , app              = express()
-  , staticMiddleware = express.static(__dirname + '/www');
+   ,app              = express()
+   ,staticMiddleware = express.static(__dirname + '/www');
 
 app.configure(function() {
     app.use(staticMiddleware);
 });
+console.log("server run at localhost:3000");
 app.listen(3000);
